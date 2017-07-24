c87d13db7ee6efa7e699830e2ad2c79cb4e6c589
ylhope <858131544@qq.com>
2017-07-24 22:59:35 +0800

    7-14

---
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeActivity.java
@@ -0,0 +1,17 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CrimeActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime);
+
+        FragmentManager fm = getSupportFragmentManager();
+    }
+}
